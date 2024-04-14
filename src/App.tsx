a8076b920f1a23fe56399fbc1a44d7e898f52d49
Lucas da Silva <lucasdasilvadev1@gmail.com>
2024-04-14 19:17:55 -0300

    feat: :sparkles: Implement search employee by name, job or phone

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,21 @@ import { getEmployees } from "./services";
 
 function App() {
   const [employees, setEmployees] = useState<IEmployee[]>([]);
+  const [search, setSearch] = useState<string>("");
 
   useEffect(() => {
     getEmployees().then((data) => setEmployees(data));
   }, []);
+
+  const filteredEmployees =
+    search.length > 0
+      ? employees.filter(
+          ({ name, job, phone }) =>
+            name.toLowerCase().includes(search.toLowerCase()) ||
+            job.toLowerCase().includes(search.toLowerCase()) ||
+            phone.toLowerCase().includes(search.toLowerCase())
+        )
+      : employees;
 
   return (
     <main>
@@ -25,11 +36,15 @@ function App() {
               className="search-input"
               type="text"
               placeholder="Pesquisar"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
             />
             <img className="search-icon" src={SearchIcon} alt="Search" />
           </div>
         </div>
-        {employees.length > 0 && <EmployeeTable employees={employees} />}
+        {employees.length > 0 && (
+          <EmployeeTable employees={filteredEmployees} />
+        )}
       </div>
     </main>
   );
