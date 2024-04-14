e4817713215d94ca3c26476cb5ec173d617f482b
Lucas da Silva <lucasdasilvadev1@gmail.com>
2024-04-14 12:40:38 -0300

    feat: :sparkles: Adds title and search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Header } from "./components";
 import "./App.css";
+import SearchIcon from "./assets/search.svg";
 
 function App() {
   return (
-    <main className="main-card">
+    <main>
+      <Header />
       <div className="content-card">
-        <Header />
+        <div className="title-card">
+          <h1 className="title">Funcionários</h1>
+
+          <div className="search-card">
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Pesquisar"
+            />
+            <img className="search-icon" src={SearchIcon} alt="Search" />
+          </div>
+        </div>
       </div>
     </main>
   );
