d8ee2a12b8e6c9c0cc244bb21c79b208c3c62f0f
Lucas da Silva <lucasdasilvadev1@gmail.com>
2024-04-15 10:53:52 -0300

    refactor: :recycle: Create EmployeeFactory fuction to create and format employee

--- src/services/employeesService.ts
+++ src/services/employeesService.ts
@@ -1,7 +1,31 @@
 import { IEmployee } from "../interfaces/IEmployee";
+import { formatDate } from "./formatDate";
+import { formatPhoneNumber } from "./formatPhoneNumber";
 
 const API_URL = "http://localhost:3001";
 
+type EmployeeFromApi = {
+  id: number;
+  name: string;
+  job: string;
+  admissionDate: string;
+  phone: string;
+  image: string;
+};
+
+const EmployeeFactory = (employees: EmployeeFromApi[]): IEmployee[] => {
+  return employees.map((employee) => ({
+    id: employee.id,
+    name: employee.name,
+    job: employee.job,
+    admissionDate: formatDate(employee.admissionDate),
+    phone: formatPhoneNumber(employee.phone),
+    image: employee.image,
+  }));
+};
+
 export async function getEmployees(): Promise<IEmployee[]> {
-  return fetch(API_URL + "/employees").then((res) => res.json());
+  return fetch(API_URL + "/employees")
+    .then((res) => res.json())
+    .then((employees: EmployeeFromApi[]) => EmployeeFactory(employees));
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,1 @@
 export { getEmployees } from "./employeesService";
-export { formatPhoneNumber } from "./formatPhoneNumber";
-export { formatDate } from "./formatDate";
